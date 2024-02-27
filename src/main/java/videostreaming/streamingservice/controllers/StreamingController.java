e4debeea5eeaf64a7a44a97dc0853d70515664af
Ayham-Alhittini <ayham.hittini268@gmail.com>
2024-02-27 18:10:57 +0300

    edit origin access

--- src/main/java/videostreaming/streamingservice/controllers/StreamingController.java
+++ src/main/java/videostreaming/streamingservice/controllers/StreamingController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/streaming")
+@CrossOrigin(origins = "*")
 public class StreamingController {
 
     private final VideoRepository videoRepository;
